81106af3ba24c7c719c6ba76f2e69ce02b45f32d
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-10-31 18:47:42 +0600

    create a google & github sign in button by react-icons

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { FaGoogle, FaGithub } from 'react-icons/fa';
 
 const RightNav = () => {
-    return (
-        <div>
-            <h2>Right Nav</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Login With</h2>
+      <Button className="mb-2" variant="outline-primary"> <FaGoogle></FaGoogle>  Sign in With Google</Button>
+      <Button variant="outline-secondary"> <FaGithub></FaGithub>  Sign in With GitHub</Button>
+
+      <div>
+        <h4 className="mt-2">Find Us On</h4>
+      </div>
+
+
+
+    </div>
+  );
 };
 
 export default RightNav;
