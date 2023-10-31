d0274c63a218152d8d497611c916e6f69e1049a8
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-10-31 18:59:59 +0600

    create social medis icons by react icons

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import { FaGoogle, FaGithub } from 'react-icons/fa';
+import { Button, ListGroup } from "react-bootstrap";
+import { FaGoogle, FaGithub,FaFacebook,FaTwitter,FaInstagram } from 'react-icons/fa';
 
 const RightNav = () => {
   return (
@@ -9,8 +9,18 @@ const RightNav = () => {
       <Button className="mb-2" variant="outline-primary"> <FaGoogle></FaGoogle>  Sign in With Google</Button>
       <Button variant="outline-secondary"> <FaGithub></FaGithub>  Sign in With GitHub</Button>
 
-      <div>
+      <div >
         <h4 className="mt-2">Find Us On</h4>
+        {/* <ListGroup >
+      <ListGroup.Item > <FaFacebook></FaFacebook> Facebook  </ListGroup.Item>
+      <ListGroup.Item > <FaTwitter></FaTwitter> Twitter </ListGroup.Item>
+      <ListGroup.Item> <FaInstagram></FaInstagram> Instagram </ListGroup.Item>
+     
+    </ListGroup> */}
+      <Button variant="outline-secondary"> <FaFacebook></FaFacebook>  Facebook</Button>
+      <Button variant="outline-secondary"> <FaTwitter></FaTwitter> Twitter</Button>
+      <Button variant="outline-secondary"> <FaInstagram></FaInstagram>  Instagram </Button>
+
       </div>
 
 
