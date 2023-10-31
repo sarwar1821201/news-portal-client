bbb627aaca0ecc6b2cf574ba801d97fb160bf2fb
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-10-31 18:06:02 +0600

    set a information portal like breaking news by using marquee

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
-import moment from 'moment';
+import moment from "moment";
+import { Button, Container } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 
 const Header = () => {
   return (
-    <div>
+    <Container>
       <div className="text-center">
         <img src={logo} alt="" />
         <p className="text-secondary">
           <small>Journalism Without fear and favour</small>
-          
         </p>
         <p>{moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}</p>
       </div>
-    </div>
+
+      <div className="d-flex">
+        <Button variant="danger">Latest</Button>
+        <Marquee className="text-danger" speed={50} >
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nesciunt a animi ullam possimus nulla numquam! Voluptate quidem sint illum voluptatem. Natus est eum nulla aperiam veritatis magni perspiciatis adipisci libero sequi, assumenda amet omnis facilis suscipit impedit ex inventore. Perferendis suscipit, magnam placeat reiciendis quas quam ut optio aut.
+        </Marquee>
+      </div>
+    </Container>
   );
 };
 
