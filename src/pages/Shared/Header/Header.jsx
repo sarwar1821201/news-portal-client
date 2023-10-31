aa7c240c379727b54e38f53853a291a3c29b2bd1
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-10-31 18:17:51 +0600

    create a navbar

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../../../assets/logo.png";
 import moment from "moment";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
 
 const Header = () => {
@@ -21,6 +21,30 @@ const Header = () => {
          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nesciunt a animi ullam possimus nulla numquam! Voluptate quidem sint illum voluptatem. Natus est eum nulla aperiam veritatis magni perspiciatis adipisci libero sequi, assumenda amet omnis facilis suscipit impedit ex inventore. Perferendis suscipit, magnam placeat reiciendis quas quam ut optio aut.
         </Marquee>
       </div>
+
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Container>
+        
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">About</Nav.Link>
+            <Nav.Link href="#features">Career</Nav.Link>
+           
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Profile</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+             <Button variant="secondary">Login</Button>
+
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+
     </Container>
   );
 };
