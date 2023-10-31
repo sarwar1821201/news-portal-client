4428dbe397a12353b5099917319a7b3c3d550168
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-10-31 17:03:26 +0600

    show logo on the header of the page

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import logo from "../../../assets/logo.png";
 
 const Header = () => {
-    return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
-    );
+  return (
+    <div>
+      <div className="text-center">
+        <img src={logo} alt="" />
+        <p className="text-secondary">
+          <small>Journalism Without fear and favour</small>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
