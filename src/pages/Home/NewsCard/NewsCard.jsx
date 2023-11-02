9dc8f72c947bc5fc6e5e011b807bf0b7cbe83ff3
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-11-02 14:00:17 +0600

    style header of the card-component

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import { Card, Image } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import moment from "moment";
+import React from "react";
+import { Card, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const NewsCard = ({news}) => {
-    const {_id,title,details, image_url,author}= news
-    return (
-        <Card className=" mb-4">
-        <Card.Header>
-           <Image src= {author.img} roundedCircle ></Image>
-            </Card.Header>
+const NewsCard = ({ news }) => {
+  const { _id, title, details, image_url, author } = news;
+  return (
+    <Card className=" mb-4">
+      <Card.Header className="d-flex">
+        <Image
+          style={{ height: "40px" }}
+          src={author?.img}
+          roundedCircle
+        ></Image>
+        <div className="ps-3">
+            <p className="mb-0">{author?.name}</p>
+            <p><small> {moment(author?.published_date).format('yyyy-MM-D')} </small></p>
+        </div>
+      </Card.Header>
 
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Img variant="top" src={image_url} />
-          <Card.Text>
-           {details.length <250 ? <>{details}</> : <>{details.slice(0,250)}..... <Link className='text-danger' to={`/news/${_id}`}  >Read More</Link> </>  }
-          </Card.Text>
-        
-        </Card.Body>
-        <Card.Footer className="text-muted">2 days ago</Card.Footer>
-      </Card>
-    );
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Text>
+          {details.length < 250 ? (
+            <>{details}</>
+          ) : (
+            <>
+              {details.slice(0, 250)}.....{" "}
+              <Link className="text-danger" to={`/news/${_id}`}>
+                Read More
+              </Link>{" "}
+            </>
+          )}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+    </Card>
+  );
 };
 
 export default NewsCard;
