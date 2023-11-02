be86484ff79c8f4385ec147ac3126b763d32a585
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-11-02 14:29:01 +0600

    react eye icon and total view

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,11 +1,12 @@
 import moment from "moment";
 import React from "react";
 import { Card, Image } from "react-bootstrap";
-import { FaRegBookmark, FaShareAlt } from "react-icons/fa";
+import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from "react-icons/fa";
+import Rating from "react-rating";
 import { Link } from "react-router-dom";
 
 const NewsCard = ({ news }) => {
-  const { _id, title, details, image_url, author } = news;
+  const { _id, title, details, image_url, author, total_view,rating } = news;
   return (
     <Card className=" mb-4">
       <Card.Header className="d-flex align-items-center">
@@ -40,7 +41,25 @@ const NewsCard = ({ news }) => {
           )}
         </Card.Text>
       </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      <Card.Footer className="text-muted d-flex">
+
+        <div className="flex-grow-1">
+            <Rating
+            placeholderRating={rating.number}
+            readonly
+            emptySymbol={<FaRegStar></FaRegStar>}
+            placeholderSymbol={<FaStar className="text-warning" ></FaStar>}
+            fullSymbol={<FaStar></FaStar>}
+            ></Rating>
+
+         <span>{rating.number}</span>
+        </div>
+
+        <div>
+        <FaEye></FaEye>{total_view}
+        </div>
+
+      </Card.Footer>
     </Card>
   );
 };
