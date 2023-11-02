ee2ffbb696a317e15ffcc527af9af25346b45357
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-11-02 14:04:44 +0600

    add fontawesome icon on the card header components

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -1,21 +1,26 @@
 import moment from "moment";
 import React from "react";
 import { Card, Image } from "react-bootstrap";
+import { FaRegBookmark, FaShareAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const NewsCard = ({ news }) => {
   const { _id, title, details, image_url, author } = news;
   return (
     <Card className=" mb-4">
-      <Card.Header className="d-flex">
+      <Card.Header className="d-flex align-items-center">
         <Image
           style={{ height: "40px" }}
           src={author?.img}
           roundedCircle
         ></Image>
-        <div className="ps-3">
+        <div className="ps-3 flex-grow-1">
             <p className="mb-0">{author?.name}</p>
             <p><small> {moment(author?.published_date).format('yyyy-MM-D')} </small></p>
+        </div>
+
+        <div>
+            <FaRegBookmark></FaRegBookmark> <FaShareAlt></FaShareAlt>
         </div>
       </Card.Header>
 
