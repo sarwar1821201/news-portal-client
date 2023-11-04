cf8934d91f7b2e801887818d17054b06afafa34b
sarwar1821201 <sarwardipo1821201@gmail.com>
2023-11-04 11:33:23 +0600

    editors insights on the news route page

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Row } from 'react-bootstrap';
 import { FaArrowLeft } from 'react-icons/fa';
 import { Link, useLoaderData } from 'react-router-dom';
+import EditorInsights from '../EditorInsights/EditorInsights';
 
 const News = () => {
     const news= useLoaderData()
     const { _id, title, details, image_url, category_id } = news;
     return (
-        <Card>
+      <div>
+
+<Card>
         <Card.Img variant="top" src={image_url} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
@@ -17,6 +20,14 @@ const News = () => {
          <Link to={`/category/${category_id}`} > <Button variant="danger"> <FaArrowLeft></FaArrowLeft>  All News in this Category</Button></Link>
         </Card.Body>
       </Card>
+      
+      <EditorInsights></EditorInsights>
+
+      </div>
+
+    // editors insights
+   
+
     );
 };
 
